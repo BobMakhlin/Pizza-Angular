fdd7a3c588b7a632b5853935d7083527956a4dd9
Bob Makhlin <borismakhlin5@gmail.com>
2020-12-02 21:33:54 +0200

    Add urls

---
+++ src/app/data/urls.ts
@@ -0,0 +1,13 @@
+const urls = {
+  apis: {
+    pizza: {
+      root: 'https://pizzastep.pp.ua:44306/api',
+      
+      categories: 'https://pizzastep.pp.ua:44306/api/category',
+      products: 'https://pizzastep.pp.ua:44306/api/product',
+      statuses: 'https://pizzastep.pp.ua:44306/api/status',
+      ingredients: 'https://pizzastep.pp.ua:44306/api/ingredient',
+      orders: 'https://pizzastep.pp.ua:44306/api/order',
+    }
+  }
+}
